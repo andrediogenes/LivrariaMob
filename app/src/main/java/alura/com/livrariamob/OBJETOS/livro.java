13435203b0a33240f2fe49b0dc91ddabf716b14f
André Diógenes <andrediosilva@gmail.com>
2022-06-21 10:51:41 -0400

    criando os bancos

---
+++ app/src/main/java/alura/com/livrariamob/OBJETOS/livro.java
@@ -0,0 +1,54 @@
+package alura.com.livrariamob.OBJETOS;
+
+public class livro {
+    Integer livro_id;
+    Integer usuario_cadastro_id;
+    String livro_nome;
+    String livro_genero;
+    String livro_autor;
+    Float livro_preco;
+
+    public Integer getLivro_id() {
+        return livro_id;
+    }
+
+    public Integer getUsuario_cadastro_id() {
+        return usuario_cadastro_id;
+    }
+
+    public void setUsuario_cadastro_id(Integer usuario_cadastro_id) {
+        this.usuario_cadastro_id = usuario_cadastro_id;
+    }
+
+    public String getLivro_nome() {
+        return livro_nome;
+    }
+
+    public void setLivro_nome(String livro_nome) {
+        this.livro_nome = livro_nome;
+    }
+
+    public String getLivro_genero() {
+        return livro_genero;
+    }
+
+    public void setLivro_genero(String livro_genero) {
+        this.livro_genero = livro_genero;
+    }
+
+    public String getLivro_autor() {
+        return livro_autor;
+    }
+
+    public void setLivro_autor(String livro_autor) {
+        this.livro_autor = livro_autor;
+    }
+
+    public Float getLivro_preco() {
+        return livro_preco;
+    }
+
+    public void setLivro_preco(Float livro_preco) {
+        this.livro_preco = livro_preco;
+    }
+}
---
+++ app/src/main/java/alura/com/livrariamob/OBJETOS/venda.java
@@ -0,0 +1,49 @@
+package alura.com.livrariamob.OBJETOS;
+
+public class venda {
+    Integer venda_id;
+    Integer venda_id_usuario;
+    Integer venda_id_livro;
+    String venda_data;
+    String venda_forma_pagamento;
+
+    public Integer getVenda_id() {
+        return venda_id;
+    }
+
+    public void setVenda_id(Integer venda_id) {
+        this.venda_id = venda_id;
+    }
+
+    public Integer getVenda_id_usuario() {
+        return venda_id_usuario;
+    }
+
+    public void setVenda_id_usuario(Integer venda_id_usuario) {
+        this.venda_id_usuario = venda_id_usuario;
+    }
+
+    public Integer getVenda_id_livro() {
+        return venda_id_livro;
+    }
+
+    public void setVenda_id_livro(Integer venda_id_livro) {
+        this.venda_id_livro = venda_id_livro;
+    }
+
+    public String getVenda_data() {
+        return venda_data;
+    }
+
+    public void setVenda_data(String venda_data) {
+        this.venda_data = venda_data;
+    }
+
+    public String getVenda_forma_pagamento() {
+        return venda_forma_pagamento;
+    }
+
+    public void setVenda_forma_pagamento(String venda_forma_pagamento) {
+        this.venda_forma_pagamento = venda_forma_pagamento;
+    }
+}
